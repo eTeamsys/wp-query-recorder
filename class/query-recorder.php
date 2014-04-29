3601fe7d166737b47e71e88550edbc681a462c61
Chris Aprea <chris.aprea@gmail.com>
2014-04-29 16:19:20 +1000

    Options page #1

--- class/query-recorder.php
+++ class/query-recorder.php
@@ -2,6 +2,9 @@
 class Query_Recorder {
 
 	function __construct( $plugin_file_path ) {
+		$this->set_default_options();
+		$this->load_options();
+
 		$this->plugin_version = $GLOBALS['query_recorder_version'];
 		$this->plugin_file_path = $plugin_file_path;
 		$this->plugin_dir_path = plugin_dir_path( $plugin_file_path );
@@ -24,7 +27,7 @@ class Query_Recorder {
 	}
 
 	function add_pages() {
-		$options_page = add_options_page( __( "Query Recorder", 'query-recorder' ), __( "Query Recorder", 'query-recorder' ), $this->required_cap, 'query-recorder', array( $this, 'page_options' ) );
+		$options_page = add_options_page( __( "Query Recorder Options", 'query-recorder' ), __( "Query Recorder", 'query-recorder' ), $this->required_cap, 'query-recorder', array( $this, 'page_options' ) );
 	}
 
 	function plugin_action_links( $links ) {
@@ -35,32 +38,87 @@ class Query_Recorder {
 	}
 
 	function record_query( $sql ) {
-		if ( !preg_match( '@^(INSERT|UPDATE|DELETE|DROP|CREATE)@i', $sql ) ) {
-			return $sql;
+		if ( !empty( $this->options['record_queries_beggining_with'] ) ) {
+			$record_queries_beggining_with = implode( '|', $this->options['record_queries_beggining_with'] );
+			if ( !preg_match( '@^(' . $record_queries_beggining_with . ')@i', $sql ) ) {
+				return $sql;
+			}
 		}
 
-		$ignore_strings = array( 
-			"_transient",
-			"`option_name` = 'cron'",
-			"post_type=deprecated_log",
-			"SET `comment_count`",
-			"`meta_key` = '_edit_lock'",
-			"_yoast_wpseo_linkdex"
-		);
-		foreach ( $ignore_strings as $string ) {
+		foreach ( $this->options['exclude_queries'] as $string ) {
 			if ( false !== strpos( $sql, $string ) ) {
 				return $sql;
 			}
 		}
 
 		$upload_dir = wp_upload_dir();
-		file_put_contents( $upload_dir['basedir'] . '/recorded-queries.txt', $sql . "\n", FILE_APPEND );
+		file_put_contents( $this->options['saved_queries_file_path'], $sql . "\n", FILE_APPEND );
 
 		return $sql;
 	}
 
-	function page_options() {
+	function set_default_options() {
+		// default option for "Save queries to file"
+		$upload_dir = wp_upload_dir();
+		$salt = strtolower( wp_generate_password( 5, false, false ) );
+		$saved_queries_file_path = sprintf( '%srecorded-queries-%s.sql', trailingslashit( $upload_dir['basedir'] ), $salt );
+		$this->default_options['saved_queries_file_path'] = $saved_queries_file_path;
+	
+		// default option for "Exclude queries containing"
+		$this->default_options['exclude_queries'] = array( '_transient', '`option_name` = \'cron\'' );
 
+		// default option for "Record queries that begin with"
+		$this->default_options['record_queries_beggining_with'] = array( 'INSERT', 'UPDATE', 'DELETE', 'DROP', 'CREATE' );
+	}
+
+	function load_options() {
+		$update_options = false;
+
+		$this->options = get_option( 'query_recorder' );
+
+		// if no options exist then this is a fresh install, set up some default options
+		if ( empty( $this->options ) ) {
+			$this->options = $this->default_options;
+			$update_options = true;
+		} else {
+			$this->options = wp_parse_args( $this->options, $this->default_options );
+		}
+
+		if ( $update_options ) {
+			update_option( 'query_recorder', $this->options );
+		}
+
+		// allow developers to change the options regardless of the stored values
+		$this->options = apply_filters( 'query_recorder_options', $this->options );		
+	}
+
+	function update_options() {
+		$_POST = stripslashes_deep( $_POST );
+
+		$this->options['saved_queries_file_path'] = trim( $_POST['saved_queries_file_path'] );
+		$this->options['exclude_queries'] = explode( "\n", trim( $_POST['exclude_queries'] ) );
+		$this->options['record_queries_beggining_with'] = $_POST['record_queries_beggining_with'];
+
+		update_option( 'query_recorder', $this->options );
+
+		echo '<div id="message" class="updated fade"><p>' . __( 'Options saved.', 'query-recorder' ) . '</p></div>';
+	}
+
+	function page_options() {
+		if ( 'POST' == $_SERVER['REQUEST_METHOD'] ) {
+			check_admin_referer( 'query_recorder_update_options' );
+			$this->update_options();
+		}
+
+		extract( $this->options, EXTR_SKIP );
+
+		// these types of queries can be recorded, others cannot
+		$recordable_queries = apply_filters( 'query_recorder_recordable_queries', array( 'INSERT', 'UPDATE', 'DELETE', 'DROP', 'CREATE' ) );
+
+		// process the content for the "Exclude queries containing" textarea
+		$exclude_queries = ( empty( $exclude_queries ) ) ? '' : implode( "\n", $exclude_queries );
+
+		require_once $this->plugin_dir_path . 'template/options.php';
 	}
 
 }
---
+++ template/options.php
@@ -0,0 +1,51 @@
+<div class="wrap query-recorder">
+	<h2><?php _e( 'Query Recorder Options', 'query-recorder' ); ?></h2>
+
+	<form method="post" action="">
+		<?php wp_nonce_field( 'query_recorder_update_options' ); ?>
+		<table class="form-table">
+			<tbody>
+				<tr>
+					<th scope="row">
+						<label for="saved-queries-file-path"><?php _e( "Save queries to file", "query-recorder" ); ?></label>
+					</th>
+					<td>
+						<input type="text" class="regular-text" value="<?php echo $saved_queries_file_path; ?>" id="saved-queries-file-path" name="saved_queries_file_path" />
+					</td>
+				</tr>
+				<tr>
+					<th scope="row">
+						<label for="exclude-queries"><?php _e( "Don't record queries containing", "query-recorder" ); ?></label>
+					</th>
+					<td>
+						<p>
+							<textarea id="exclude-queries" cols="50" rows="10" name="exclude_queries"><?php echo $exclude_queries; ?></textarea>
+							<p class="description"><?php _e( "One exclusion per line.", "query-recorder" ); ?></p>
+						</p>
+					</td>
+				</tr>
+				<tr>
+					<th scope="row">
+						<?php _e( "Record queries that begin with", "query-recorder" ); ?>
+					</th>
+					<td>
+						<fieldset>
+							<?php foreach( $recordable_queries as $keyword ) : ?>
+								<?php $keyword_lower = strtolower( $keyword ); ?>
+								<label for="<?php echo $keyword_lower; ?>">
+									<input type="checkbox" value="<?php echo $keyword; ?>" id="<?php echo $keyword_lower; ?>" name="record_queries_beggining_with[]"<?php echo ( !empty( $record_queries_beggining_with ) && in_array( $keyword, $record_queries_beggining_with ) ) ? ' checked="checked"' : ''; ?> />
+									<span class="code"><?php echo $keyword; ?></span>
+								</label><br />
+							<?php endforeach; ?>
+						</fieldset>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+
+		<p class="submit">
+			<input type="submit" value="<?php _e( 'Save Changes', 'query-recorder' ); ?>" class="button button-primary" id="submit" name="submit">
+		</p>
+	</form>
+
+</div>
