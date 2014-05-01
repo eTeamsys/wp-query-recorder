410672b53d30447b41b0a178bee8acca269db236
Chris Aprea <chris.aprea@gmail.com>
2014-05-01 10:58:53 +1000

    Undefined index when all checkboxes were unchecked

--- class/query-recorder.php
+++ class/query-recorder.php
@@ -148,7 +148,7 @@ class Query_Recorder {
 
 		$this->options['saved_queries_file_path'] = trim( $_POST['saved_queries_file_path'] );
 		$this->options['exclude_queries'] = explode( "\n", trim( $_POST['exclude_queries'] ) );
-		$this->options['record_queries_beggining_with'] = $_POST['record_queries_beggining_with'];
+		$this->options['record_queries_beggining_with'] = isset( $_POST['record_queries_beggining_with'] ) ? $_POST['record_queries_beggining_with'] : array();
 
 		update_option( 'query_recorder', $this->options );
 
